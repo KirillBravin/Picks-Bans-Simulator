312a5524cbe54d6838efe41e472c2fd157d36761
Phil <kshaadoo1991@gmail.com>
2023-02-25 12:27:57 +0200

    Picking multiple bans

--- js/script.js
+++ js/script.js
@@ -4,17 +4,53 @@ let   wrapperBansTeamOne = document.querySelector('.tm-bans-1'),
         wrapperPicksTeamTwo = document.querySelector('.picks-tm-2'),
         wrapperChampions = document.querySelector('.champion-pull');
 
+let countBanTeam1 = 0;
+let countBanTeam2 = 0;
+let countPickTeam1 = 0;
+let countPickTeam2 = 0;
+let count = 0;
 
-let banTeam1 = function (){
+function banTeam1 (){
     wrapperChampions.addEventListener('click', (e) => {
-        if (e.target && e.target.tagName == "BUTTON") {
-            for (let i = 0; i < wrapperChampions.childElementCount; i++){
-                    if(wrapperChampions.children[i] == e.target){
-                        wrapperBansTeamOne.children[0].classList.add(wrapperChampions.children[i].classList[1]);
-                    }
-            }
+        if (countBanTeam1 > 5){
+            return;
+        }
+         else if (countBanTeam1 < 5){
+            if (e.target && e.target.tagName == "BUTTON") {
+                for (let i = 0; i < wrapperChampions.childElementCount; i++){
+                        if(wrapperChampions.children[i] == e.target){
+                            wrapperBansTeamOne.children[countBanTeam1].classList.add(wrapperChampions.children[i].classList[1]);
+                        }
+                }
+                countBanTeam1++;
+                return banTeam1();
+             } 
          }
     }, {once: true});
-};
+}
+
+function banTeam2 (){
+    wrapperChampions.addEventListener('click', (e) => {
+        if (countBanTeam2 > 5){
+            return;
+        }
+         else if (countBanTeam2 < 5){
+            if (e.target && e.target.tagName == "BUTTON") {
+                for (let i = 0; i < wrapperChampions.childElementCount; i++){
+                        if(wrapperChampions.children[i] == e.target){
+                            wrapperBansTeamTwo.children[countBanTeam2].classList.add(wrapperChampions.children[i].classList[1]);
+                        }
+                }
+                countBanTeam2++;
+                console.log(countBanTeam2);
+                return banTeam2();
+             } 
+         }
+    }, {once: true});
+}
 
 banTeam1();
+
+function banning (){
+    
+}
