62ba63cf5ae6e4a39fba664c1a59f42ae9712170
Phil <kshaadoo1991@gmail.com>
2023-02-25 11:37:08 +0200

    Pictures Fixed

--- js/script.js
+++ js/script.js
@@ -4,28 +4,17 @@ let   wrapperBansTeamOne = document.querySelector('.tm-bans-1'),
         wrapperPicksTeamTwo = document.querySelector('.picks-tm-2'),
         wrapperChampions = document.querySelector('.champion-pull');
 
-/* console.dir(wrapperBansTeamOne.children[0]);
-console.dir(wrapperChampions.children[0]);
-wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[0].outerHTML; */
 
-
-/* wrapperChampions.addEventListener('click', (e) => {
-    if (e.target && e.target.tagName == "BUTTON") {
-        for (let i = 0; i < wrapperChampions.childElementCount; i++){
-            if(wrapperChampions.children[i] == e.target){
-                console.log('asd');
-                wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[i].outerHTML;
+let banTeam1 = function (){
+    wrapperChampions.addEventListener('click', (e) => {
+        if (e.target && e.target.tagName == "BUTTON") {
+            for (let i = 0; i < wrapperChampions.childElementCount; i++){
+                    if(wrapperChampions.children[i] == e.target){
+                        wrapperBansTeamOne.children[0].classList.add(wrapperChampions.children[i].classList[1]);
+                    }
             }
-        }
-    }
-}); */
+         }
+    }, {once: true});
+};
 
-wrapperChampions.addEventListener('click', (e) => {
-    if (e.target && e.target.tagName == "BUTTON") {
-        for (let i = 0; i < wrapperChampions.childElementCount; i++){
-                if(wrapperChampions.children[i] == e.target){
-                    wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[i].outerHTML;
-                }
-        }
-     }
-}, {once: true});
+banTeam1();
