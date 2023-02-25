13ba88255d39f529d5a4d28c1aef30a8a297635c
Phil <kshaadoo1991@gmail.com>
2023-02-25 10:06:50 +0200

    Banning champs ver 1.0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-const   banButtonsTeamOne = document.querySelectorAll('.banTeam1'),
-        banButtonsTeamTwo = document.querySelectorAll('.banTeam2'),
-        pickButtonsTeamOne = document.querySelectorAll('.pickTeam1'),
-        pickButtonsTeamTwo = document.querySelectorAll('.pickTeam2'),
-        champions = document.querySelectorAll('.champion');
+let   wrapperBansTeamOne = document.querySelector('.tm-bans-1'),
+        wrapperBansTeamTwo = document.querySelector('.tm-bans-2'),
+        wrapperPicksTeamOne = document.querySelector('.picks-tm-1'),
+        wrapperPicksTeamTwo = document.querySelector('.picks-tm-2'),
+        wrapperChampions = document.querySelector('.champion-pull');
 
-const listenClick = () => {
-    console.log('click');
-};
+/* console.dir(wrapperBansTeamOne.children[0]);
+console.dir(wrapperChampions.children[0]);
+wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[0].outerHTML; */
 
-banButtonsTeamOne.forEach(btn => {
-    btn.addEventListener('click', listenClick);
-});
+
+/* wrapperChampions.addEventListener('click', (e) => {
+    if (e.target && e.target.tagName == "BUTTON") {
+        for (let i = 0; i < wrapperChampions.childElementCount; i++){
+            if(wrapperChampions.children[i] == e.target){
+                console.log('asd');
+                wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[i].outerHTML;
+            }
+        }
+    }
+}); */
+
+wrapperChampions.addEventListener('click', (e) => {
+    if (e.target && e.target.tagName == "BUTTON") {
+        for (let i = 0; i < wrapperChampions.childElementCount; i++){
+                if(wrapperChampions.children[i] == e.target){
+                    wrapperBansTeamOne.children[0].outerHTML = wrapperChampions.children[i].outerHTML;
+                }
+        }
+     }
+}, {once: true});
